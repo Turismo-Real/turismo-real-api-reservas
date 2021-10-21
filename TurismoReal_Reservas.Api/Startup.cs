9c25cbc0a9d72f3db88c20c440bfda5733d8c068
Enzo Norese <enzo.norese@gmail.com>
2021-10-21 02:47:01 -0300

    Se eliminan dependencias innecesarias

--- TurismoReal_Reservas.Api/Startup.cs
+++ TurismoReal_Reservas.Api/Startup.cs
@@ -1,15 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using TurismoReal_Reservas.Core.Interfaces;
 using TurismoReal_Reservas.Infra.Repositories;
 
